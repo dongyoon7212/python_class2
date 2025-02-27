a5dd0213edd865b212940e091832c797f03ed357
dongyoon7212 <dongyoon7212@naver.com>
2025-02-27 19:42:47 +0900

    Feat: day5,6

---
+++ day05.py
@@ -0,0 +1,47 @@
+#대입 연산자
+#x = 10 # 대입
+#x += 10 # 더하고 대입(할당) x = x + 10
+#x -= 5 # 빼고 대입(할당) x = x - 5
+#x *= 3 # 곱하고 대입(할당) x = x * 3
+#x /= 2 # 나누고(몫이 실수) 대입(할당) x = x / 2
+#x //= 2 # 나누고(몫이 정수) 대입(할당) x = x // 2
+#print(x)
+from doctest import script_from_examples
+
+#비교연산자
+# x = 10
+# y = 20
+# z = 10
+# print(x == y) #같다
+# print(x != z) #같지 않다
+# print(x > y) #왼쪽기준 오른쪽보다 크다
+# print(x < y) #왼쪽기준 오른쪽보다 작다
+# print(x >= z) #크거나 같다
+# print(x <= y) #작거나 같다
+
+#논리연산자
+# a = True
+# b = False
+# print(a and b)
+# print(a or (a and b))
+# print(not a)
+# print(not a and b)
+
+#조건연산자 (삼항연산자)
+a = 10
+b = 20
+# max_value = a if a > b else b
+# print(max_value)
+if a > b:
+    max_value = a
+else:
+    max_value = b
+score = 85
+grade = "A" if score >= 90 else ("B" if score >= 80 else "C")
+print(grade)
+if score >= 90:
+    print("A")
+elif score >= 80:
+    print("B")
+else:
+    print("C")
---
+++ day06.py
@@ -0,0 +1,39 @@
+#제어문
+"""
+조건문
+반복문 (for, while)
+"""
+
+#조건문
+# num = int(input("숫자를 입력해 주세요"))
+# if num > 0:
+#     print("양수입니다.")
+# else:
+#     print("음수입니다.")
+
+# input_str = input("점수를 입력해 주세요")
+# if input_str.isdigit():
+#     if int(input_str) >= 90:
+#         print("A 입니다.")
+#     elif int(input_str) >= 80:
+#         print("B 입니다.")
+#     elif int(input_str) >= 70:
+#         print("C 입니다.")
+#     else:
+#         print("D 입니다.")
+# else:
+#     print("숫자가 아닙니다.")
+
+#숫자를 입력받고 짝수인지 홀수인지
+# num = input("숫자를 입력해 주세요:")
+# if num % 2 == 0:
+#     print("짝수")
+# else:
+#     print("홀수")
+#나이랑 학생인지 아닌지 두가지 질문
+#성인이면서 학생이면 성인학생입니다
+#성인학생이 아닙니다
+age = input("나이를 입력하세요")
+student = input("학생입니까?(y/n)")
+
+
