ada9c5735a32907c91076ea9465853e705b1528e
dongyoon7212 <dongyoon7212@naver.com>
2025-03-04 21:48:43 +0900

    Feat: mini project

--- 가위바위보 게임.py
+++ 가위바위보 게임.py
@@ -1,52 +1,50 @@
 import random
 import time
 
-choices = ["가위", "바위", "보"]
-computer_score = 0
-user_score = 0
-
-print("\n가위바위보 게임을 시작하겠습니다.\n")
-
-
+choice_list = ["가위", "바위", "보"]
 user_choice = ""
+computer_choice = ""
+user_score = 0
+computer_score = 0
 
 while True:
-    if computer_score == 5: # 컴퓨터가 5번 먼저 이겼을때
-        print(f"컴퓨터 승리! 컴퓨터 - {computer_score}, 사용자 - {user_score}")
+    if user_score == 5:
+        print("사용자 승리~!!!!")
         break
-    elif user_score == 5: # 사용자가 5번 먼저 이겼을때
-        print(f"사용자 승리! 컴퓨터 - {computer_score}, 사용자 - {user_score}")
-        break
-
-    print("==================현재점수===================")
-    print(f"컴퓨터 - {computer_score}, 사용자 - {user_score}") # 매 라운드 마다 현재 점수 출력
-    print("==========================================\n")
-    computer_choice = random.choice(choices) # 랜덤으로 컴퓨터의 선택
-
-    user_choice = input("가위, 바위, 보 중 선택해 주세요.(그만하려면 종료를 입력해주세요) : ") #선택 입력받기
-    if user_choice == "종료": # 만약 종료를 입력했을때
-        print("===================종료====================")
-        print(f"컴퓨터 점수:{computer_score}, 사용자 점수:{user_score}")
-        print("==========================================\n")
+    elif computer_score == 5:
+        print("컴퓨터 승리~!!!!")
         break
 
-    if user_choice not in choices and user_choice != "종료": # 만약 선택이 잘못되었을때(선택 유효성 검사)
-        print("가위, 바위, 보 또는 종료 중 하나 선택해야 합니다.\n")
+    user_choice = input("가위, 바위, 보 중 선택해 주세요(그만하려면 종료):")
+
+    if user_choice not in choice_list and user_choice != "종료":
+        print("가위, 바위, 보 중 다시 선택해 주세요")
         continue
 
-    print("\n과연 승부는......?")
-    time.sleep(2) # 2초 지연
-    if computer_choice == user_choice: #컴퓨터의 선택과 사용자의 선택이 같을때
-        print("\n무승부 하셨습니다.")
-        continue
-    elif (computer_choice == "가위" and user_choice == "바위") or \
-        (computer_choice == "바위" and user_choice == "보") or \
-        (computer_choice == "보" and user_choice == "가위"): # 사용자가 이기는 경우
-        # (computer_choice == "가위" and user_choice == "바위") or (computer_choice == "바위" and user_choice == "보") or (computer_choice == "보" and user_choice == "가위"):
-        print(f"\n이겼습니다!")
+    if user_choice == "종료":
+        print(f"종료되었습니다. 컴퓨터 - {computer_score} 사용자 - {user_score}")
+        break
+
+    computer_choice = random.choice(choice_list)
+
+    print("과연 승부는 ...........")
+    time.sleep(2)
+
+    if user_choice == computer_choice:
+        print("무승부 입니다.")
+    elif (user_choice == "가위" and computer_choice == "보") or\
+          (user_choice == "바위" and computer_choice == "가위") or\
+          (user_choice == "보" and computer_choice == "바위"):
+        print("이겼습니다!!!")
         user_score += 1
-        continue
-    else: #나머지 컴퓨터가 이기는 경우
-        print("\n졌습니다ㅜㅜ")
-        computer_score += 1
-        continue
+    else:
+        print("졌습니다 ㅜㅜ")
+        computer_score +=1
+
+    print(f"컴퓨터 - {computer_score} 사용자 - {user_score}")
+    continue
+
+
+
+
+
--- 숫자맞추기 게임.py
+++ 숫자맞추기 게임.py
@@ -1,57 +1,63 @@
 import random
 
-difficulty_list = ["쉬움", "보통", "어려움"] #난이도 리스트
-difficulty = "" # 입력받을 난이도 변수
-max_try = 0 #최대 시도 횟수
-max_range = 0 #최대 숫자 범위
+#난이도 선택 유효성 검사를 위한 난이도 리스트
+difficulty_list  = ["쉬움", "보통", "어려움"]
+try_count = 0 #시도 횟수 변수
+guess = 0 #내가 추측한 숫자 변수
+difficulty = "" #난이도를 입력하는 변수
+max_try_count = 0 #최대 시도 횟수 변수
+max_range = 0 #랜덤 숫자 최대 범위 변수
 
-while True: #난이도 선택 유효성 검사
-    difficulty = input("난이도를 선택해 주세요. (쉬움, 보통, 어려움) : ")
+#난이도 선택 유효성 검사
+while True:
+    #난이도 입력 받음
+    difficulty = input("난이도를 선택해 주세요(쉬움, 보통, 어려움)")
+    #만약에 입력받은 난이도가 난이도 리스트 안에 있는 요소 중 하나이면
     if difficulty in difficulty_list:
         break
     else:
-        print("쉬움, 보통, 어려움 중에 선택해 주세요.")
+        #난이도 리스트에 없는 입력값 -- 재입력
+        print("다시 난이도를 입력해 주세요")
         continue
 
-if difficulty == "쉬움": #쉬움을 택했을때 난이도 설정
-    max_try = 10
+# 난이도 선택때 설정되는 최대 시도횟수, 최대범위
+if difficulty == "쉬움":
+    max_try_count = 10
     max_range = 50
-elif difficulty == "보통": #보통을 택했을때 난이도 설정
-    max_try = 7
+elif difficulty == "보통":
+    max_try_count = 7
     max_range = 70
-else: #어려움을 택했을때 난이도 설정
-    max_try = 5
+else:
+    max_try_count = 5
     max_range = 100
 
-correct_answer = random.randint(1, max_range) #1 ~ 최대 숫자 범위까지 랜덤 숫자 생성
-try_count = 0 #시도한 횟수
-
-print(f"숫자 맞추기 게임을 시작하겠습니다. 난이도 : {difficulty}(1 ~ {max_range}, 최대 시도 횟수-{max_try})") #난이도와 범위 출력
-
+#선택된 난이도에 따라 랜덤 숫자 최대 범위는 달라진다
+correct_answer = random.randint(1,max_range)
+print(f"선택된 난이도-{difficulty} 랜덤 범위-1~{max_range} 최대 시도 횟수-{max_try_count}")
+#======================================================
+#여기가 본체
 while True:
-    print(f"시도 횟수 : {try_count} / {max_try}") # 시도 횟수 출력
-    if try_count >= max_try: #시도 횟수 유효성 검사
-        print("시도 횟수를 초과하였습니다!")
-        print(f"정답 : {correct_answer}")
+    #시도 횟수와 최대 시도 횟수가 같아지면 게임 종료
+    if try_count == max_try_count:
+        print(f"최대 시도 횟수에 도달했습니다. 게임 종료! 정답은 {correct_answer}")
         break
 
-    input_str = input("숫자를 맞춰보세요 : ") # 문자열로 입력값을 받음
-    if input_str.isdigit(): # 숫자 입력 유효성 검사
+    #숫자 입력에 대한 유효성 검사
+    input_str = input("숫자를 입력해 주세요 : ") #문자열
+    if input_str.isdigit():
         guess = int(input_str)
-    else: #문자 입력시 다시 처음부터 입력받음
-        print("숫자로 입력해주세요!")
+    else:
+        print("숫자로 입력해 주세요!!!")
         continue
 
-    if guess > max_range:
-        print("범위 내의 숫자를 입력해 주세요")
-        continue
+    #게임 진행
+    try_count += 1
+    print(f"시도 횟수 : {try_count}")
 
-    try_count += 1 #시도한 횟수 추가
-
-    if correct_answer > guess: #만약 정답이 입력한 숫자보다 클 경우
-        print("UP!")
-    elif correct_answer < guess: #만약 정답이 입력한 숫자보다 작을 경우
-        print("DOWN!")
-    else: #만약 정답일 경우
-        print(f"정답입니다! {try_count}번 만에 맞추셨어요!")
+    if correct_answer > guess:
+        print("UP")
+    elif correct_answer < guess:
+        print("DOWN")
+    else:
+        print(f"정답입니다!! 정답 : {correct_answer}")
         break
